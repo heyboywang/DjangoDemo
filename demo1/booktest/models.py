12152b06ad51af5edc42c83c4bb895741dff2d44
wangshuo <670681219@qq.com>
2019-04-16 11:57:34 +0800

    创建第一个Django项目中的模型

--- demo1/booktest/models.py
+++ demo1/booktest/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Bookinfo(models.Model):
+    btitle = models.CharField(max_length=255)
+    bpub_date = models.DateTimeField()
+
+class HeroInfo(models.Model):
+    hname = models.CharField(max_length=20)
+    hgender = models.BooleanField()
+    hcontent = models.CharField(max_length=100)
+    hBook = models.ForeignKey('Bookinfo',on_delete=models.CASCADE)
