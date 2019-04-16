865d08ad665d610710a70e2cb8229ff188c10c71
wangshuo <670681219@qq.com>
2019-04-16 20:32:32 +0800

    启动后台服务器，管理界面

--- demo1/booktest/admin.py
+++ demo1/booktest/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Bookinfo,HeroInfo
+from demo1.booktest.models import Bookinfo,HeroInfo
 
 # Register your models here.
 admin.site.register(Bookinfo)
