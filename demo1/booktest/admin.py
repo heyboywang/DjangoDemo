3ffce29e240046b03f952d9c2dbf0311bf3bf4d4
wangshuo <670681219@qq.com>
2019-04-16 16:17:48 +0800

    启动后台服务器，管理界面

--- demo1/booktest/admin.py
+++ demo1/booktest/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Bookinfo,HeroInfo
 
 # Register your models here.
+admin.site.register(Bookinfo)
+admin.site.register(HeroInfo)
--- demo1/booktest/models.py
+++ demo1/booktest/models.py
@@ -4,10 +4,14 @@ from django.db import models
 
 class Bookinfo(models.Model):
     btitle = models.CharField(max_length=255)
-    bpub_date = models.DateTimeField()
+    bpub_date = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.btitle
 
 class HeroInfo(models.Model):
     hname = models.CharField(max_length=20)
     hgender = models.BooleanField()
     hcontent = models.CharField(max_length=100)
     hBook = models.ForeignKey('Bookinfo',on_delete=models.CASCADE)
+    def __str__(self):
+        return self.hname
