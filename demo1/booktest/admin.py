69c56fb491ddf2d4a79c3a5e89a521f3a2d84eaa
wangshuo <670681219@qq.com>
2019-04-17 11:47:56 +0800

    服务器端管理界面显示

--- demo1/booktest/admin.py
+++ demo1/booktest/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
-from demo1.booktest.models import Bookinfo,HeroInfo
+from .models import Bookinfo,HeroInfo
+
+class HeroInfoInline(admin.StackedInline):
+    model = HeroInfo
+    extra = 1
+
+
 
 # Register your models here.
-admin.site.register(Bookinfo)
-admin.site.register(HeroInfo)
+class BookinfoAdmin(admin.ModelAdmin):
+    list_display = ['btitle','bpub_date']
+    inlines = [HeroInfoInline]
+
+class HeroInfoAdmin(admin.ModelAdmin):
+    #显示字段
+    list_display = ['hname','hgender','hcontent']
+    #过滤字段
+    list_filter = ['hgender','hcontent']
+    #搜索字段
+    search_fields = ['hname','hgender','hcontent']
+    #分页显示
+    list_per_page = 3
+
+
+admin.site.register(Bookinfo,BookinfoAdmin)
+admin.site.register(HeroInfo,HeroInfoAdmin)
