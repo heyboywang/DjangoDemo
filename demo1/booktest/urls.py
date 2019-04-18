82fecfdbe2c2265c3367da13475aaddb8bf7f997
wangshuo <670681219@qq.com>
2019-04-18 13:57:27 +0800

    增加了添加角色功能

--- demo1/booktest/urls.py
+++ demo1/booktest/urls.py
@@ -6,8 +6,9 @@ app_name = "booktest"
 urlpatterns = [
     url(r'^index/$',views.index,name="index"),
     # url(r'^detail/$',views.detail)
-    url(r'^detail/(\d+)/$',views.detail,name="detail"),
     url(r'^list/$',views.list,name="list"),
+    url(r'^detail/(\d+)/$',views.detail,name="detail"),
     url(r'^delete/(\d+)/$',views.delete,name="delete"),
-
+    url(r'^addHero/(\d+)/$',views.addHero,name="addHero"),
+    url(r'^addherotool/(\d+)$',views.addherotool,name="addherotool")
 ]
--- demo1/booktest/views.py
+++ demo1/booktest/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Bookinfo
+from django.http import HttpResponse,HttpResponseRedirect
+from .models import Bookinfo,HeroInfo
 from django.template import loader
 
 # Create your views here.
@@ -12,19 +12,8 @@ def index(request):
     # cont = {'username':'wangshuo'}
     # result = temp.render(cont)
     # return HttpResponse(result)
+    #渲染
     return render(request,'booktest/index.html',{'username':'wangshuo'})
-
-
-# def detail(request):
-#     return HttpResponse("详情页")
-def detail(request,i):
-    # try:
-    #     book = Bookinfo.objects.get(pk = int(i))
-    #     return HttpResponse(book)
-    # except:
-    #     return HttpResponse("没有此书")
-    book = Bookinfo.objects.get(pk=int(i))
-    return render(request,'booktest/detail.html',{'bookinfo':book})
 
 
 def list(request):
@@ -36,10 +25,50 @@ def list(request):
     book = Bookinfo.objects.all()
     return render(request,'booktest/list.html',{'booklist':book})
 
+
+# def detail(request):
+#     return HttpResponse("详情页")
+def detail(request,i):
+    # try:
+    #     book = Bookinfo.objects.get(pk = int(i))
+    #     return HttpResponse(book)
+    # except:
+    #     return HttpResponse("没有此书")
+    book = Bookinfo.objects.get(pk=int(i))
+    return render(request,'booktest/detail.html',{'book':book})
+
 def delete(request,i):
     try:
         Bookinfo.objects.get(pk = int(i)).delete()
         book = Bookinfo.objects.all()
-        return render(request, 'booktest/list.html', {'booklist': book})
+        #渲染
+        # return render(request, 'booktest/list.html', {'booklist': book})
+        #重定向
+        return HttpResponseRedirect("/list/",{'booklist': book})
     except:
         return HttpResponse("删除失败")
+
+def addHero(request,i):
+    book = Bookinfo.objects.get(pk=int(i))
+    return render(request,'booktest/addhero.html', {'book': book})
+
+def addherotool(request,i):
+    book = Bookinfo.objects.get(pk=int(i))
+    hname = request.POST['hname']
+    res = request.POST['sex']
+    if res == 1:
+        hgender = True
+    else:
+        hgender = False
+    hcontent = request.POST['hcontent']
+
+    h = HeroInfo()
+    h.hname = hname
+    h.hgender = hgender
+    h.hcontent = hcontent
+    h.hBook = book
+    h.save()
+    return HttpResponseRedirect('/detail/'+str(i)+'/',{'book': book})
+
+    # print(hname,hgender,hcontent,hBook_id)
+    # return HttpResponse("添加成功")
